43145e9e43dd9e4bda215320400562e53b2320ef
Gerard Larkin <gerardjlarkin@gmail.com>
2025-06-27 15:38:21 +0100

    file cleanup

--- zzArchive/bernoulli.py
+++ zzArchive/bernoulli.py
@@ -1,3 +1,16 @@
-import numpy as np
-from scipy.special import bernoulli, zeta
-bernoulli(4)
+# import numpy as np
+# from scipy.special import bernoulli, zeta
+# ouput = bernoulli(1000)
+# from time import perf_counter, process_time, clock_gettime_ns, CLOCK_REALTIME
+
+# test_time = clock_gettime_ns(CLOCK_REALTIME)
+
+# print(int(str(test_time)[-1]))
+
+import random
+import string
+
+# get random password pf length 8 with letters, digits, and symbols
+characters = string.ascii_letters + string.digits
+password = ''.join(random.choice(characters) for i in range(6))
+print("Random password is:", password)
